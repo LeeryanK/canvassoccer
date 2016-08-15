a4ac83a8ae9db9fd05a7165e3c1453940d1d6afc
Kyle Lin <kylejlin.us@gmail.com>
2016-08-14 18:59:28 -0700

    Created Circle class and added .getDifferenceFrom method to Vector class.

--- js/canvassoccer.js
+++ js/canvassoccer.js
@@ -26,6 +26,7 @@
     return new Vector(Math.cos(angle) * magnitude, Math.sin(angle) * magnitude);
   };
 
+
   Vector.ZERO = new Vector(0, 0);
 
   Vector.prototype.clone = function() {
@@ -52,6 +53,10 @@
     return Math.atan(this.y / this.x);
   };
 
+  Vector.prototype.getDifferenceFrom = function(otherVector) {
+    return new Vector(otherVector.x - this.x, otherVector.y - this.y);
+  };
+
   function Movable(pos, vel, acc) {
     this.position = pos || Vector.ZERO.clone();
     this.velocity = vel || Vector.ZERO.clone();
@@ -63,5 +68,45 @@
 
     this.velocity.add(this.acceleration.clone().multiply(multiplier));
     this.position.add(this.velocity.clone().multiply(multiplier));
+  };
+
+  var Shapes = {
+    CIRCLE: 0,
+    RECTANGLE: 1
+  };
+
+  function Circle(pos, vel, acc, radius) {
+    Movable.call(this, pos, vel, acc);
+    this.radius = radius;
+  }
+
+  Circle.prototype = Object.create(Movable.prototype);
+
+  Circle.prototype.shapeType = Shapes.CIRCLE;
+
+  Circle.prototype.isTouching = function(otherShape) {
+    switch (otherShape.shapeType) {
+      case Shapes.CIRCLE:
+        return (this.radius + otherShape.radius) >= this.position.getDifferenceFrom(otherShape.position).getMagnitude();
+
+      case Shapes.RECTANGLE:
+        // Created rectangular collision box with padding equal to the radius of
+        //   this circle.
+        var ox = otherShape.position.x;
+        var oy = otherShape.position.y;
+        var ow = otherShape.width;
+        var oh = otherShape.height;
+
+        var x = this.position.x;
+        var y = this.position.y;
+        var r = this.radius;
+
+        var et = oy - r;
+        var er = ox + ow + r;
+        var eb = oy + oh + r;
+        var el = ox - r;
+
+        return x < er && x > el && y > et && y < eb;
+    }
   };
 })();
